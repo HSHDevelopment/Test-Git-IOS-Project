cfb31d2656c22182021c2e82be4174f7ed024929
Luis Eduardo Vieira Mendes <luiseduardovieiramendes@Luiss-MacBook-Pro.local>
2014-06-09 22:29:44 +0200

    added Test Class

---
+++ GIT Test/HSHTest.h
@@ -0,0 +1,13 @@
+//
+//  HSHTest.h
+//  GIT Test
+//
+//  Created by Luis Eduardo Vieira Mendes on 09/06/14.
+//  Copyright (c) 2014 Home Sweet Home Dev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HSHTest : NSObject
+
+@end
---
+++ GIT Test/HSHTest.m
@@ -0,0 +1,13 @@
+//
+//  HSHTest.m
+//  GIT Test
+//
+//  Created by Luis Eduardo Vieira Mendes on 09/06/14.
+//  Copyright (c) 2014 Home Sweet Home Dev. All rights reserved.
+//
+
+#import "HSHTest.h"
+
+@implementation HSHTest
+
+@end
